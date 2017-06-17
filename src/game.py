8c16eb228443f9e4918839eb078e3d2ee8aa9d94
Bo Pace <bopace@gmail.com>
2017-06-16 23:55:25 -0600

    groundwork for dungeon generation
    created classes and functions to create basic dungeons in preparation
    for random dungeon generation

--- src/game.py
+++ src/game.py
@@ -11,30 +11,6 @@ color_dark_wall = libtcod.Color(0, 0, 100)
 color_dark_ground = libtcod.Color(50, 50, 150)
 
 
-def handle_keys():
-	global playerx, playery
-
-	key = libtcod.console_check_for_keypress()
-	if key.vk == libtcod.KEY_ENTER and key.lalt:
-		# Alt+Enter: toggle fullscreen
-		libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
-
-	elif key.vk == libtcod.KEY_ESCAPE:
-		return True		# exit game
-
-	#movement keys
-	if libtcod.console_is_key_pressed(libtcod.KEY_UP):
-		player.move(0, -1)
-
-	elif libtcod.console_is_key_pressed(libtcod.KEY_DOWN):
-		player.move(0, 1)
-
-	elif libtcod.console_is_key_pressed(libtcod.KEY_LEFT):
-		player.move(-1, 0)
-
-	elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
-		player.move(1, 0)
-
 
 class Object:
 	# Generic object used for various game features
@@ -72,18 +48,55 @@ class Tile:
 		self.block_sight = block_sight
 
 
+class Rect:
+	# Rectangle, used to represent a room
+	def __init__(self, x, y, w, h):
+		self.x1 = x
+		self.y1 = y
+		self.x2 = x + w
+		self.y2 = y + h
+
+
+def handle_keys():
+	global playerx, playery
+
+	key = libtcod.console_check_for_keypress()
+	if key.vk == libtcod.KEY_ENTER and key.lalt:
+		# Alt+Enter: toggle fullscreen
+		libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
+
+	elif key.vk == libtcod.KEY_ESCAPE:
+		return True		# exit game
+
+	#movement keys
+	if libtcod.console_is_key_pressed(libtcod.KEY_UP):
+		player.move(0, -1)
+
+	elif libtcod.console_is_key_pressed(libtcod.KEY_DOWN):
+		player.move(0, 1)
+
+	elif libtcod.console_is_key_pressed(libtcod.KEY_LEFT):
+		player.move(-1, 0)
+
+	elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
+		player.move(1, 0)
+
+
 def make_map():
 	global map
 
-	# fill map with unblocked tiles
-	map = [[ Tile(False)
+	# fill map with blocked tiles
+	map = [[ Tile(True)
 		for y in range(MAP_HEIGHT) ]
 			for x in range(MAP_WIDTH) ]
 
-	map[30][22].blocked = True
-	map[30][22].block_sight = True
-	map[50][22].blocked = True
-	map[50][22].block_sight = True
+	# create two rooms
+	room1 = Rect(20, 15, 10, 15)
+	room2 = Rect(50, 15, 10, 15)
+	create_room(room1)
+	create_room(room2)
+
+	create_h_tunnel(25, 55, 23)
 
 
 def render_all():
@@ -103,6 +116,29 @@ def render_all():
 	libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
 
 
+def create_room(room):
+	global map
+	# make tiles within rectangle able to be traversed
+	for x in range(room.x1 + 1, room.x2):
+		for y in range(room.y1 + 1, room.y2):
+			map[x][y].blocked = False
+			map[x][y].block_sight = False
+
+
+def create_h_tunnel(x1, x2, y):
+	global map
+	for x in range(min(x1, x2), max(x1, x2) + 1):
+		map[x][y].blocked = False
+		map[x][y].block_sight = False
+
+
+def create_v_tunnel(y1, y2, x):
+	global map
+	for y in range(min(y1, y2), max(y1, y2) + 1):
+		map[x][y].blocked = False
+		map[x][y].block_sight = False
+
+
 ########################################################
 # Window setup and main game loop
 ########################################################
@@ -117,6 +153,8 @@ npc	   = Object(SCREEN_WIDTH/2 - 5, SCREEN_HEIGHT/2, '@', libtcod.yellow)
 objects = [npc, player]
 
 make_map()
+player.x = 25
+player.y = 23
 
 while not libtcod.console_is_window_closed():
 
